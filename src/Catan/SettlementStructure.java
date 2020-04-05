3d797d87398af935e8a984dbefe095d494b6528d
Jakob Braunschweiger <jbraunschweiger@wustl.edu>
2020-04-05 01:54:05 -0500

    fixed settlement city switchup

--- src/Catan/SettlementStructure.java
+++ src/Catan/SettlementStructure.java
@@ -4,14 +4,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class SettlementStructure extends Structure {
-	static StructureType type = StructureType.CITY;
+	static StructureType type = StructureType.SETTLEMENT;
 	static Map<CardType, Integer> cost;
 	static {
 		cost = new HashMap<CardType, Integer>();
-		cost.put(CardType.WHEAT, 2);
-		cost.put(CardType.ORE, 3);
+		cost.put(CardType.BRICK, 1);
+		cost.put(CardType.LUMBER, 1);
+		cost.put(CardType.WHEAT, 1);
+		cost.put(CardType.SHEEP, 1);
 	}
-	static int resourceYield = 2;
+	static int resourceYield = 1;
 	
 	public SettlementStructure(Player o) {
 		super(o);
