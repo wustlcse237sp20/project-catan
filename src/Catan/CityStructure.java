4d10941ed39c3c0ee881acacccaa128ca3272ad8
Jakob Braunschweiger <jbraunschweiger@wustl.edu>
2020-04-05 01:49:51 -0500

    implemented settlement

---
+++ src/Catan/CityStructure.java
@@ -0,0 +1,10 @@
+package Catan;
+
+public class CityStructure extends Structure {
+
+	public CityStructure(Player o) {
+		super(o);
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- src/Catan/SettlementStructure.java
+++ src/Catan/SettlementStructure.java
@@ -4,16 +4,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class SettlementStructure extends Structure {
-	static StructureType type = StructureType.SETTLEMENT;
+	static StructureType type = StructureType.CITY;
 	static Map<CardType, Integer> cost;
 	static {
 		cost = new HashMap<CardType, Integer>();
-		cost.put(CardType.BRICK, 1);
-		cost.put(CardType.LUMBER, 1);
-		cost.put(CardType.WHEAT, 1);
-		cost.put(CardType.SHEEP, 1);
+		cost.put(CardType.WHEAT, 2);
+		cost.put(CardType.ORE, 3);
 	}
-	static int resourceYield = 1;
+	static int resourceYield = 2;
 	
 	public SettlementStructure(Player o) {
 		super(o);
