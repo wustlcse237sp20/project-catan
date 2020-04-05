6dfdbeb75215033cbccf707534ca60deb01cb38d
Jakob Braunschweiger <jbraunschweiger@wustl.edu>
2020-04-05 04:51:26 -0500

    added functionality to player class

--- src/Catan/Player.java
+++ src/Catan/Player.java
@@ -3,24 +3,52 @@ package Catan;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.HashMap;
+import java.util.ArrayList;
 
 public class Player {
-	Map<CardType, Integer> hand;
+	Map<CardType, Integer> handCardAmounts;
+	ArrayList<DevelopmentCard> devCards;
 	PlayerType type;
 	int victoryPoints = 0;
 	
 	public Player(PlayerType t) {
-		hand = new HashMap<CardType, Integer>();
-		hand.put(CardType.BRICK, 0);
-		hand.put(CardType.LUMBER, 0);
-		hand.put(CardType.WHEAT, 0);
-		hand.put(CardType.ORE, 0);
-		hand.put(CardType.SHEEP, 0);
-		hand.put(CardType.DEVELOPMENT, 0);
+		handCardAmounts = new HashMap<CardType, Integer>();
+		handCardAmounts.put(CardType.BRICK, 0);
+		handCardAmounts.put(CardType.LUMBER, 0);
+		handCardAmounts.put(CardType.WHEAT, 0);
+		handCardAmounts.put(CardType.ORE, 0);
+		handCardAmounts.put(CardType.SHEEP, 0);
+		handCardAmounts.put(CardType.DEVELOPMENT, 0);
 		
 		type = t;
 	}
 	
+	// for adding resource card(s) when you have the card type but no Card instance
+	public void addCardsToHand(CardType cardType, int amount) {
+		int newAmount = handCardAmounts.get(cardType) + amount;
+		handCardAmounts.replace(cardType, newAmount);
+		
+	}
+	
+	// for removing cards given a type
+	public void removeCardsFromHand(CardType cardType, int amount) {
+		int newAmount = handCardAmounts.get(cardType) - amount;
+		if(newAmount < 0) {
+			newAmount = 0;
+			System.out.println("something went wrong, subtracted too many cards from hand");
+		}
+		handCardAmounts.replace(cardType, newAmount);
+	}
+	
+	// for adding an instance to the players hand
+	public void addOneCardToHand(Card newCard) {
+		this.addCardsToHand(newCard.type, 1);
+		if(newCard instanceof DevelopmentCard) {
+			devCards.add((DevelopmentCard)newCard);
+		}
+	}
+	
+	// Structure Factory
 	public Structure buildStructure(StructureType model) {
 		Structure newStructure = null;
 		switch(model) {
@@ -42,12 +70,28 @@ public class Player {
 		return null;
 	}
 	
+	// Card Factory
+	public void buildCard(CardType model) {
+		Card newCard = null;
+		switch(model) {
+		case DEVELOPMENT:
+			newCard = new DevelopmentCard();
+			break;
+		default:
+			newCard = new ResourceCard(model);
+		}
+		if(this.purchase(newCard)) {
+			this.addOneCardToHand(newCard);
+		}
+	}
+	
+	// generic purchase method to handle transactions
 	private boolean purchase(Purchasable p) {
-		if(p.canPlayerAfford(hand)) {
+		if(p.canPlayerAfford(handCardAmounts)) {
 			Map<CardType, Integer> cost = p.getCost();
 			for(Entry<CardType, Integer> costValue: cost.entrySet()){
 				CardType key = costValue.getKey();
-				hand.replace(key, hand.get(key) - cost.get(key));
+				handCardAmounts.replace(key, handCardAmounts.get(key) - cost.get(key));
 			}
 			return true;
 		}
