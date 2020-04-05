86da60014cc58c2db91e5b11fe38465cfb857bb9
Jakob Braunschweiger <jbraunschweiger@wustl.edu>
2020-04-05 03:52:39 -0500

    implemented Card superclass

--- src/Catan/Card.java
+++ src/Catan/Card.java
@@ -1,5 +1,17 @@
 package Catan;
 
-public interface Card {
+import java.util.Map;
+
+public abstract class Card implements Purchasable {
+	CardType type;
+	
+	public Card(CardType model) {
+		type = model;
+	}
+
+	@Override
+	public abstract boolean canPlayerAfford(Map<CardType, Integer> hand);
+	@Override
+	public abstract Map<CardType, Integer> getCost();
 
 }
