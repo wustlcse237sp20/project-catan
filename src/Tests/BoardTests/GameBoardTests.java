f45d8791678b8db58545546cb873f963b791e6bc
466726 (Evan Zhou) <46686487+ehzhou@users.noreply.github.com>
2020-04-06 05:56:48 -0400

    Edited genMap/genBoard code for clarity

---
+++ src/Tests/BoardTests/GameBoardTests.java
@@ -0,0 +1,22 @@
+package Tests.BoardTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.awt.Color;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import catan.Board.StdDraw;
+import catan.Board.Tile;
+
+
+import org.junit.jupiter.api.Test;
+
+class GameBoardTests {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
