94edeac7f410608497e470cfb8971a0e38aac766
466726 (Evan Zhou) <46686487+ehzhou@users.noreply.github.com>
2020-04-20 00:33:32 -0400

    Implemented hexagonal grid instead of hard-coded draw. Work done by Andy but commited by Evan due to IDE issues

--- src/catan/Tile.java
+++ src/catan/Tile.java
@@ -1,8 +1,8 @@
 package catan;
 import java.awt.Color;
+import java.awt.Point;
 
 public class Tile {
-
 	private double centerX;
 	private double centerY;
 	private int value;
@@ -10,11 +10,18 @@ public class Tile {
 	private double width;
 	private boolean hasRobber;
 	
-	
+	/**
+	 * Initializes Tile with the following parameters:
+	 * @param x x-coordinate of center of hexagon tile
+	 * @param y y-coordinate of center of hexagon tile
+	 * @param tileValue number associated with the tile: if dice rolls this number, tile is "activated"
+	 * @param tileColor
+	 * @param tileWidth
+	 */
 	public Tile(double x, double y, int tileValue, Color tileColor, double tileWidth) {
-		centerX =x;
+		centerX = x;
 		centerY = y;
-		value =tileValue;
+		value = tileValue;
 		color = tileColor;
 		width = tileWidth;
 		if (tileValue==0) {
@@ -23,6 +30,10 @@ public class Tile {
 			hasRobber = false;
 		}
 	}
+	
+	/**
+	 * Draws a hexagonal tile based on its x/y coordinates, color, and value
+	 */
 	public void drawTile() {
 		double hypotenuse = Math.sqrt(2);
 		double multiple = width*hypotenuse;
@@ -43,11 +54,40 @@ public class Tile {
 				
 			}
 	}
+	
 	public boolean hasRobber() {
 		return hasRobber;
 	}
+	
+	public Point.Double getPoint() {
+		Point.Double pointDouble = new Point.Double(centerX, centerY);
+		return pointDouble;
+	}
+	public int comparePoint (Point.Double compared) {
+		if(compared.x >  centerX) {
+			return -1;
+		} else if (compared.x <  centerX) {
+			return 1;
+		} else {
+			if(compared.y >  centerY) {
+				return -1;
+			} else if (compared.y <  centerY) {
+				return 1;
+			} else {
+				return 0;
+			}
+			
+			
+		}
+		
+	}
+	
 	public void setRobber(boolean newValue) {
 		hasRobber = newValue;
+	}
+  public static void main(String[] args) {
+		// TODO Auto-generated method stub 
+      System.out.println("SDFSDF");
 	}
 }
 
